5129a8761af776242fb5590a6c82920c074e27ef
EmanMH <eman.mahmoud89@gmail.com>
2019-06-02 01:40:19 +0200

    Lab5-updated

--- Labs/5/randomUser/routes/users.js
+++ Labs/5/randomUser/routes/users.js
@@ -1,19 +1,31 @@
 var express = require('express');
 var router = express.Router();
 const axios =require('axios')
-const {from} =require(' rxjs');
-const {shareReplay}=rxjs.operators;
+const {from} = require("rxjs");
+const {shareReplay}= require("rxjs/operators");
+const url = require('url');
 
 var randomUserApi="https://randomuser.me/api/";
+
+const getFullUrl = function (req, query) {
+  return url.format({
+    protocol: req.protocol,
+    host: req.get('host'),
+    pathname: req.originalUrl,
+    query: query
+  });
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
  let pathStr="?results=";
 
- var obs$= from(axios.get(randomUserApi+pathStr));
+ var obs$= from(axios.get(randomUserApi+pathStr)).pipe(shareReplay(1));
 
- obs$.subscribe(data=>console.log(data));
+ obs$.subscribe(data=> res.set('Cache-Control', 'private, max-age=86400').set("Link"
+ , `<${getFullUrl(req, {page: parseInt(req.query.page || 0) + 1})}>; rel="next"`)
+  .json(data.data));
 
-  res.send('respond with a resource');
 });
 
 module.exports = router;
